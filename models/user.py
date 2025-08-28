3467cae5795d978f8c12c83267082a749889a5f6
raulkm108 <raulkm.civil@gmail.com>
2025-08-28 08:20:49 -0300

    removed pass line

--- models/user.py
+++ models/user.py
@@ -6,4 +6,3 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, primary_key=True)
     password = db.Column(db.String(20), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user')
-    pass
