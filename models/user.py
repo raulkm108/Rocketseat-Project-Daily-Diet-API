335cd9aefb551db18da9441b4435f94eb786fbab
raulkm108 <raulkm.civil@gmail.com>
2025-08-28 08:19:08 -0300

    Implemented User attributes
    Those beeing the id, username and password, all going to the db

--- models/user.py
+++ models/user.py
@@ -2,4 +2,7 @@ from flask_login import UserMixin
 from database import db
 
 class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, primary_key=True)
+    password = db.Column(db.String(20), nullable=False)
     pass
