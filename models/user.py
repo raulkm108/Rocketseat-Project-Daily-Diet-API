4a42d35fb606596ea13bd43c6442ad14134dde43
raulkm108 <raulkm.civil@gmail.com>
2025-08-28 13:01:58 -0300

    Changed primary_key to unique
    The id is already the primary key, the username has to be unique, or the db will fail to interpreter it otherwise

--- models/user.py
+++ models/user.py
@@ -3,6 +3,6 @@ from database import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user')
