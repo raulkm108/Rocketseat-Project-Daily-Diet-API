b14cdd4078dac02cae2c88985f15b8a7d5c93c54
raulkm108 <raulkm.civil@gmail.com>
2025-08-28 08:20:33 -0300

    Added role attribute

--- models/user.py
+++ models/user.py
@@ -5,4 +5,5 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, primary_key=True)
     password = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default='user')
     pass
