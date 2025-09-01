376f5c0a0889da471466f9c41b0a8022fe1ccbb1
raulkm108 <raulkm.civil@gmail.com>
2025-09-01 14:31:19 -0300

    Corrected a few typos

--- models/user.py
+++ models/user.py
@@ -7,15 +7,15 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(20), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user')
 
-    meal = db.relationship('Meal', backref='author', lazy=True)
+    meals = db.relationship('Meal', backref='author', lazy=True)
     
 
-class Meals(db.Model):
-    id = db.Columm(db.integer, primary_key=True)
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.string(300), nullable=True)
-    mealtime = db.Column(db.string(20), nullable=True)
+    description = db.Column(db.String(300), nullable=True)
+    mealtime = db.Column(db.String(20), nullable=True)
     indiet = db.Column(db.Boolean, nullable=False)
 
-    user_id = db.Column(db.integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
