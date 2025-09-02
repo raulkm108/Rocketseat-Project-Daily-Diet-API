51dee6d44328f77a3c869236aea643177298ca66
raulkm108 <raulkm.civil@gmail.com>
2025-09-02 12:03:59 -0300

    created test py

---
+++ test.py
@@ -0,0 +1,12 @@
+from database import db
+from models.user import User, Meal
+from app import app
+
+with app.app_context():
+
+    id_user = 1
+
+    user = User.query.get(id_user)
+    for meal in user.meals:
+        pass
+    
